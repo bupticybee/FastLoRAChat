5b4bf6911c8d0cf4231c67c6cafdedcc921e8aec
wangfuming <wangfuming@bytedance.com>
2023-04-11 15:11:41 +0800

    :new: add transformers version

--- finetune_lora.sh
+++ finetune_lora.sh
@@ -1,3 +1,4 @@
+# transformers 41a2f3529c6b56866c317031375ffd3e7b8bea01
 #OMP_NUM_THREADS=8 WORLD_SIZE=2 CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=2 --master_port=9967  \
     python3 finetune.py \
     --base_model '../llama-13b/' \
